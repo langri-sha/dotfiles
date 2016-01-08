8b0d531254b379e26fd5ddd9d2532b1bdd613f97
Filip Dupanović <filip.dupanovic@gmail.com>
2016-01-08 03:14:39 +0100

    Create path

--- hooks/pre-up/tag-tmux.sh
+++ hooks/pre-up/tag-tmux.sh
@@ -5,5 +5,6 @@ tpm=$(readlink -f ../../tag-tmux)/tmux/plugins/tpm
 
 # Clone tmux-plugins/tpm
 if [ ! -e $tpm ]; then
+    mkdir -p $(dirname $tpm)
     git clone --depth 1 https://github.com/tmux-plugins/tpm $tpm
 fi
