a0c9e15f31fffdddc70ee036707bd6dfbb2d5f63
Filip Dupanović <filip.dupanovic@gmail.com>
2023-10-26 08:15:18 +0000

    Add Rust toolchain

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 if [ -f "/etc/debian_version" ]; then
     sudo apt-get update
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 else
     echo "[ERROR] Sorry, your operating system isn't supported!"
 fi
