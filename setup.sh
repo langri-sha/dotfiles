ab4476ff0743cd4844734b19e45f0aa751279179
Filip Dupanović <filip.dupanovic@gmail.com>
2023-10-27 09:30:32 +0000

    Install `google-cloud-cli`

--- setup.sh
+++ setup.sh
@@ -14,6 +14,13 @@ if [ -d "/home/codespace" ] && [ -f "/etc/debian_version" ]; then
     sudo apt-get update
     sudo apt-get install terraform
     terraform -install-autocomplete
+
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+        sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | \
+        sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    sudo apt-get update
+    sudo apt-get install google-cloud-cli
 else
     echo "[ERROR] Sorry, your environment or operating system isn't supported!"
 fi
