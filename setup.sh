6318f30dae0344a4a6ae7a3bc00d11178a462215
Filip Dupanović <filip.dupanovic@gmail.com>
2023-10-27 07:59:46 +0000

    Add `tealdeer`

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ if [ -f "/etc/debian_version" ]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     $HOME/.cargo/bin/cargo install \
         ripgrep \
-        starship
+        starship \
+        tealdeer
 else
     echo "[ERROR] Sorry, your operating system isn't supported!"
 fi
