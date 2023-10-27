dd50664eba7d3713209931356dadc5ec77fddd52
Filip Dupanović <filip.dupanovic@gmail.com>
2023-10-27 07:58:20 +0000

    Add `ripgrep`

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 if [ -f "/etc/debian_version" ]; then
     sudo apt-get update
+
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    $HOME/.cargo/bin/cargo install \
+        ripgrep
 else
     echo "[ERROR] Sorry, your operating system isn't supported!"
 fi
